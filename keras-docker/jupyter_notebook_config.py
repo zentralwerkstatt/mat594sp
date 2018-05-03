d95e63fee90b32ff88da1a244446d35268f51aff
Fabian Offert <fabian.offert@gmail.com>
2018-05-03 13:18:24 -0700

    Minor corrections, copyright notice

--- keras-docker/jupyter_notebook_config.py
+++
@@ -1,8 +0,0 @@
-import os
-from IPython.lib import passwd
-if 'PASSWORD' in os.environ:
-  c.NotebookApp.password = passwd(os.environ['PASSWORD'])
-else:
-  c.NotebookApp.password = ''
-  c.NotebookApp.token = ''
-del os.environ['PASSWORD']
